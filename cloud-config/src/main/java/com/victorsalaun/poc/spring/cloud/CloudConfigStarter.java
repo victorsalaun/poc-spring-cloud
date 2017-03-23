98a6196aa524e5328fad399986146abfd4b3310a
victor.salaun-at-667791141321 <AiP/puwKhreAcW5wvD/7nKaWkLlIbqn2x+sb9P/mA28=>
2017-03-23 23:59:21 +0100

    add cloud config module

---
+++ cloud-config/src/main/java/com/victorsalaun/poc/spring/cloud/CloudConfigStarter.java
@@ -0,0 +1,12 @@
+package com.victorsalaun.poc.spring.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudConfigStarter {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudConfigStarter.class, args);
+	}
+}
