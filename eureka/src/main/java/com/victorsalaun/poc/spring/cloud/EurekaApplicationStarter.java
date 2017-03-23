03249a98859900975df0111714b3c6082e2a2d14
victor.salaun-at-667791141321 <AiP/puwKhreAcW5wvD/7nKaWkLlIbqn2x+sb9P/mA28=>
2017-03-24 00:01:14 +0100

    add eureka module

---
+++ eureka/src/main/java/com/victorsalaun/poc/spring/cloud/EurekaApplicationStarter.java
@@ -0,0 +1,13 @@
+package com.victorsalaun.poc.spring.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaApplicationStarter {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplicationStarter.class, args);
+    }
+
+}
